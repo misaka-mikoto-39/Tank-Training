8eebc24a86206e2547ffe6735db1e5d6b3873e8f
Misaka Mikoto <39.misaka.mikoto@gmail.com>
2021-04-28 00:24:12 +0700

    Readd BTService Player Location

--- Source/TankTraining/Private/AI/BTTask_MoveToLocation.cpp
+++ Source/TankTraining/Private/AI/BTTask_MoveToLocation.cpp
@@ -1,9 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "AI/BTTask_MoveToLocation.h"
-
-
+#include "Pawns/PawnTank.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 UBTTask_MoveToLocation::UBTTask_MoveToLocation()
 {
@@ -12,6 +11,38 @@ UBTTask_MoveToLocation::UBTTask_MoveToLocation()
 EBTNodeResult::Type UBTTask_MoveToLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	if (OwnerComp.GetAIOwner() == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+	APawnTank* Tank = Cast<APawnTank>(OwnerComp.GetAIOwner()->GetPawn());
+	if (Tank == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+	FVector TargetLocation = OwnerComp.GetBlackboardComponent()->GetValueAsVector(GetSelectedBlackboardKey());
+	/*
+	FVector LookAtTargetClean = FVector(PlayerTank->GetActorLocation().X, PlayerTank->GetActorLocation().Y, Tank->GetActorLocation().Z);
+	FVector StartLocation = Tank->GetActorLocation();
+	FRotator CameraRotation = Tank->GetCameraRotation();
+	FRotator TurretTargetRotation = FVector(LookAtTargetClean - StartLocation).Rotation();
+	if (FMath::FloorToInt(TurretTargetRotation.Yaw) != FMath::FloorToInt(CameraRotation.Yaw))
+	{
+		if (TurretTargetRotation.Yaw > CameraRotation.Yaw)
+		{
+			Tank->RotateCamera(1);
+		}
+		else
+		{
+			Tank->RotateCamera(-1);
+		}
+	}
+	else
+	{
+		//Tank->Fire();
+	}
+*/
 	UE_LOG(LogTemp, Warning, TEXT("Move to location"));
 	return EBTNodeResult::Succeeded;
 }
